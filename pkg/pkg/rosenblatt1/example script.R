80b70ff13c96cc8f7a5d3b4ec632af1635260367
johnros <johnros@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-06 15:35:56 +0000

    backup
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rosenblatt1@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pkg/rosenblatt1/example script.R
+++ pkg/pkg/rosenblatt1/example script.R
@@ -1,11 +1,18 @@
 # 
 # Author: john.ros@gmail.com
 ###############################################################################
+require(tractor.base)
+require(rosenblatt1)
+
+# Import SPM{betas} for all subjects:
+setwd('~/Projects/MRI/Data/Vink/data_vink_2010/cons_no_smooth')
+(files<- grep("con.*img", list.files(), value=T))
+(files<- sub('\\.img', "", files))
+beta.arrays<- importBetaMriImages(files, fileType='NIFTI')
+image(beta.arrays[,,20,21])
 
 
-# Import from analyze format.
-beta.arrays<- 
-		
+
 # Fit model parameters.
 mixture.fit<- mixtureFit(beta.arrays, mixtureControl=mixture.control)
 
